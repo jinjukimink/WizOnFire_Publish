15e971ad2eb358f64c4fdae6bdb7555356c47da9
jinjukim <kimjinjuind1@hanyang.ac.kr>
2024-09-04 21:40:50 +0900

    네비게이션바 에니메이션 완료

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const FooterWapper=styled.footer`
+    width: 100%;
+    height:216px;
+    background-color: blue;
+`
+const Footer = () => {
+  return (
+    <>
+      <FooterWapper/>
+    </>
+  );
+}
+export default Footer
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,22 @@
+import styled from "styled-components";
+import backgroundImage from "../assets/images/landingBackground.png";
+
+const StartImage = styled.div`
+  height: 1300px;
+  width: auto;
+  background-image: url(${backgroundImage}); /* 템플릿 리터럴을 사용하여 이미지 URL 설정 */
+  background-size:cover;
+  background-position: center;
+`;
+
 const Home = () => {
   return (
     <>
       {/* 랜딩페이지 */}
-      <h1>Home Component</h1>
+      <StartImage />
     </>
   );
 };
+
 export default Home;
+
