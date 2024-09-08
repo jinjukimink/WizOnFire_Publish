ab5107d174b84033e21bcb65a4d2172e8cf8ac55
ddu2ni <adsf9608@naver.com>
2024-09-09 06:07:00 +0900

    Design: 전체적인 css 수정

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,5 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-
-
 import { RouterProvider } from "react-router-dom";
 import router from "./router/index.tsx";
 import GlobalStyle from "./GlobalStyles.tsx";
@@ -9,9 +7,7 @@ import GlobalStyle from "./GlobalStyles.tsx";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <GlobalStyle />
-    <GlobalStyle />
     <RouterProvider router={router} />
-    {/* <App /> */}
     {/* <App /> */}
   </StrictMode>
 );
