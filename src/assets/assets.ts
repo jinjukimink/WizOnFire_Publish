9e7b06437aaea10870b85023256f24c08fbda478
1dontknowwhatyouwant <milestone0917@gmail.com>
2024-09-10 11:14:05 +0900

    구장 소개

--- src/assets/assets.ts
+++ src/assets/assets.ts
@@ -5,5 +5,6 @@ import naver from "./images/footer/naver.png"
 import youtube from "./images/footer/youtube.png"
 import subBackground from "./images/common/subBackground.png"
 import subBg from "./images/common/subBg.png"
+import wizPark from "./images/common/wizpark.jpg"
 
-export {logo_ko,facebook,instagram,naver,youtube,subBackground,subBg};
+export {logo_ko,facebook,instagram,naver,youtube,subBackground,subBg,wizPark};
