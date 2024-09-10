9da2deb3c7d009cc9ff69d008f1b0d64d31603df
1dontknowwhatyouwant <milestone0917@gmail.com>
2024-09-10 11:21:58 +0900

    feat: 구장 소개 사진 모듈화

--- src/assets/assets.ts
+++ src/assets/assets.ts
@@ -5,6 +5,6 @@ import naver from "./images/footer/naver.png"
 import youtube from "./images/footer/youtube.png"
 import subBackground from "./images/common/subBackground.png"
 import subBg from "./images/common/subBg.png"
-import wizParkStadium from "./images/common/wizpark.jpg"
+import ktWizParkStadium from "./images/common/wizpark.jpg"
 
-export {logo_ko,facebook,instagram,naver,youtube,subBackground,subBg,wizParkStadium}; 
+export {logo_ko,facebook,instagram,naver,youtube,subBackground,subBg,ktWizParkStadium}; 
