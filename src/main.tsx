71845b6b60da1ba102ff8da9be23b167feb0c56a
jinjukim <kimjinjuind1@hanyang.ac.kr>
2024-09-12 17:31:25 +0900

    d

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-
 import { RouterProvider } from "react-router-dom";
 import router from "./router/index.tsx";
 import GlobalStyle from "./GlobalStyles.tsx";
