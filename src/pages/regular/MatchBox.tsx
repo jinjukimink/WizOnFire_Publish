7683432b83bbe16ab34b2bd1f1a113ed914a81bb
jieunpark <jieun20626@gmail.com>
2024-09-16 22:33:16 +0900

    cal

--- src/pages/regular/MatchBox.tsx
+++ src/pages/regular/MatchBox.tsx
@@ -1,37 +1,92 @@
-import React from "react";
-import { DateContainer, GameBox, TeamInfo, TeamLogo, TeamsContainer, Score, MatchResult } from "./ScheduleStyles";
+import useFetchData from "../../hooks/useFetchData";
+import { useNavigate } from "react-router-dom";
+import { ContentBox, DateContainer, GameBox, MatchResult, ScheduleBox, Score, TeamInfo, TeamLogo, TeamsContainer } from "./MatchBoxStyles";
+import colors from "../../assets/Colors";
+import Button from "../../components/common/button/Button";
 
-const GameDetails = ({ game, width, height, boxShadow, dateboxColor, children, stadium, gtime }: { game: TGameData; width?: string; height?: string; boxShadow?:string; dateboxColor?: string; children?: React.ReactNode; stadium?:string; gtime?:string; }) => {
+const MatchBox = () => {
+  const navigate = useNavigate();
+  const { data : game } = useFetchData<TGameResponse>("/game/recentGames");
+  const {current, prev, next} = game?.data || {};
+  console.log('Gamedata', game);
+
   // 날짜 포맷팅 함수
   const formatDate = (date:string | undefined):string => {
     const year = date?.substring(0, 4) || "";
     const month = date?.substring(4, 6) || "";
     const day = date?.substring(6, 8) || "";
     return  `${year}.${month}.${day}`;
-  }
+}
 
   return (
-    <GameBox width={width} height={height} boxShadow={boxShadow}>
-      <DateContainer backgroundColor={dateboxColor}>{formatDate(game?.displayDate)}</DateContainer>
-      <TeamsContainer>
-        <TeamInfo>
-          <TeamLogo src={game?.visitLogo} alt="Away Team" />
-          <span>{game?.visitFullname}</span>
-          <span>W: {game?.visitStarter}</span>
-        </TeamInfo>
-        <Score>
-        {children}
-        <span>{game?.outcome}</span>
-        <span>{stadium} {gtime}</span>
-        </Score>
-        <TeamInfo>
-          <TeamLogo src={game?.homeLogo} alt="Home Team" />
-          <span>{game?.homeFullname}</span>
-          <span>L: {game?.homeStarter}</span>
-        </TeamInfo>
-      </TeamsContainer>
-    </GameBox>
-  );
-};
+    <>
+     <ScheduleBox>
+      <GameBox>
+          <DateContainer>{formatDate(prev?.displayDate)}</DateContainer>
+          <TeamsContainer>
+              <TeamInfo>
+                  <TeamLogo src={prev?.visitLogo} alt="Away Team" />
+                  <span>{prev?.visitFullname}</span>
+                  <span>L: {prev?.visitStarter}</span>
+              </TeamInfo>
+              <ContentBox>
+                <Score>{prev?.visitScore} : {prev?.homeScore}</Score>
+                <MatchResult>{prev?.outcome}</MatchResult>
+                <Button width="80px" height="25px" fontSize="13px" backgroundColor="#9e9e9e" border="none" fontColor="white" borderRadius="20px" onClick={()=>navigate("/game/regular/boxscore")}>경기 정보</Button>
+              </ContentBox>
+              <TeamInfo>
+                  <TeamLogo src={prev?.homeLogo} alt="Home Team" />
+                  <span>{prev?.homeFullname}</span>
+                  <span>W: {prev?.homeStarter}</span>
+              </TeamInfo>
+          </TeamsContainer>
+        </GameBox>
 
-export default GameDetails;
+        <GameBox width="360px" height="190px" boxShadow="0 3px 7px 0 rgba(0, 0, 0, .24)">
+          <DateContainer backgroundColor={colors.redTertiary}>{formatDate(current?.displayDate)}</DateContainer>
+          <TeamsContainer>
+              <TeamInfo>
+                  <TeamLogo src={current?.visitLogo} alt="Away Team" />
+                  <span>{current?.visitFullname}</span>
+                  <span>L: {current?.visitStarter}</span>
+              </TeamInfo>
+              <ContentBox>
+                <Score>{current?.visitScore} : {current?.homeScore}</Score>
+                <MatchResult>{current?.outcome}</MatchResult>
+                <Button width="80px" height="25px" fontSize="13px" backgroundColor="#9e9e9e" border="none" fontColor="white" borderRadius="20px" onClick={()=>navigate("/game/regular/boxscore")}>경기 정보</Button>
+              </ContentBox>
+              <TeamInfo>
+                  <TeamLogo src={current?.homeLogo} alt="Home Team" />
+                  <span>{current?.homeFullname}</span>
+                  <span>W: {current?.homeStarter}</span>
+              </TeamInfo>
+          </TeamsContainer>
+        </GameBox>
+
+        <GameBox>
+          <DateContainer>{formatDate(next?.displayDate)}</DateContainer>
+          <TeamsContainer>
+              <TeamInfo>
+                  <TeamLogo src={next?.visitLogo} alt="Away Team" />
+                  <span>{next?.visitFullname}</span>
+                  <span>선발: {next?.visitStarter}</span>
+              </TeamInfo>
+              <ContentBox> 
+                <Score>VS</Score>
+                <span>{next?.gtime} {next?.stadium} </span>
+                <Button width="80px" height="25px" fontSize="13px" backgroundColor="#9e9e9e" border="none" fontColor="white" borderRadius="20px" onClick={()=>navigate("/game/regular/boxscore")}>경기 정보</Button>
+             </ContentBox>
+              <TeamInfo>
+                  <TeamLogo src={next?.homeLogo} alt="Home Team" />
+                  <span>{next?.homeFullname}</span>
+                  <span>선발: {next?.homeStarter}</span>
+              </TeamInfo>
+          </TeamsContainer>
+        </GameBox>
+        </ScheduleBox>
+     
+      
+    </>
+  );
+}
+export default MatchBox
--- src/pages/regular/MatchBoxStyles.ts
+++ src/pages/regular/MatchBoxStyles.ts
@@ -6,7 +6,7 @@ export const ScheduleBox = styled.div`
     flex-direction: row;
     align-items: center;
     justify-content: center;
-    margin-top: 20px;
+    margin-top: 40px;
     width: 100%;
     max-width: 1100px;
     margin-left: auto; // 수평 가운데 정렬
@@ -20,8 +20,8 @@ export const GameBox = styled.div<{ width?: string; height?: string; boxShadow?:
     align-items: center;
     justify-content: center;
     border: 1px solid #d2d2d2;
-    width: ${({ width }) => width || '350px'};
-    height: ${({ height }) => height || '200px'};
+    width: ${({ width }) => width || '310px'};
+    height: ${({ height }) => height || '170px'};
     padding: 20px;
     background-color: #fff;
     box-shadow: ${({ boxShadow }) => boxShadow || 0};
@@ -30,10 +30,14 @@ export const GameBox = styled.div<{ width?: string; height?: string; boxShadow?:
 export const DateContainer = styled.div<{backgroundColor?: string}>`
     background-color: ${({backgroundColor}) => backgroundColor || colors.darkGray};
     color: white;
+    width: 80px;
+    height: 11px;
     padding: 8px 16px;
-    border-radius: 8px;
-    font-size: 16px;
-    text-align: center;
+    border-radius: 20px;
+    font-size: 15px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 `;
 
 export const TeamsContainer = styled.div`
@@ -44,8 +48,10 @@ export const TeamsContainer = styled.div`
 `;
 
 export const TeamLogo = styled.img`
-    width: 80px; /* 로고 크기 */
-    height: 80px;
+    width: 75px; 
+    height: 75px;
+    margin-left: 12px; // 수평 가운데 정렬
+    margin-right: 12px;
 `;
 
 export const TeamInfo = styled.div`
@@ -54,6 +60,7 @@ export const TeamInfo = styled.div`
     align-items: center;
     span {
         margin-bottom: 10px;
+        font-size: 14px;
         color: ${colors.ashGray};
         &:first-of-type {
             color: ${colors.black};
@@ -66,6 +73,8 @@ export const Score = styled.div`
     align-items:center;
     font-size: 30px;
     font-weight: bold;
+    margin-top: 30px;
+    margin-bottom: 15px;
     // span{
     //     margin-top:10px;
     //     font-size: 17px;
@@ -79,16 +88,15 @@ export const Score = styled.div`
 
 export const MatchResult = styled.div`
     color: ${colors.redTertiary};
+    margin-bottom: 9px;
 `;
 
-export const InfoButton = styled.button`
-    display: flex;
+export const ContentBox = styled.div`
+    display:flex;
+    flex-direction: column;
     align-items: center;
-    padding: 8px 16px;
-    background-color: #9e9e9e;
-    color: white;
-    border: none;
-    border-radius: 20px;
-    cursor: pointer;
-    font-size: 14px;
-`;
+    span{
+        font-size:14px;
+        margin-bottom:12px;
+    }
+`
