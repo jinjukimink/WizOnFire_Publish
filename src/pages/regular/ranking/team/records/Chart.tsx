bc6cc373f63df0468c6be1339ae9d36100980101
ddu2ni <adsf9608@naver.com>
2024-09-20 02:58:03 +0900

    Feat: Chart 라이브러리 npm 설치

--- src/pages/regular/ranking/team/records/Chart.tsx
+++ src/pages/regular/ranking/team/records/Chart.tsx
@@ -1,8 +1,121 @@
-const Chart = () => {
-  return (
-    <>
-      <h1>Chart Component</h1>
-    </>
-  );
+import { Component } from 'react';
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ReferenceArea,
+} from 'recharts';
+
+const initialData = [
+  { name: 1, cost: 4.11, impression: 500 },
+  { name: 2, cost: 2.39, impression: 120 },
+  { name: 3, cost: 1.37, impression: 150 },
+  { name: 4, cost: 1.16, impression: 180 },
+  { name: 5, cost: 2.29, impression: 200 },
+  { name: 6, cost: 3, impression: 499 },
+  { name: 7, cost: 0.53, impression: 50 },
+  { name: 8, cost: 2.52, impression: 100 },
+  { name: 9, cost: 1.79, impression: 200 },
+  { name: 10, cost: 2.94, impression: 222 },
+  { name: 11, cost: 4.3, impression: 210 },
+  { name: 12, cost: 4.41, impression: 300 },
+  { name: 13, cost: 2.1, impression: 50 },
+  { name: 14, cost: 8, impression: 190 },
+  { name: 15, cost: 0, impression: 300 },
+  { name: 16, cost: 9, impression: 400 },
+  { name: 17, cost: 3, impression: 200 },
+  { name: 18, cost: 2, impression: 50 },
+  { name: 19, cost: 3, impression: 100 },
+  { name: 20, cost: 7, impression: 100 },
+];
+
+const getAxisYDomain = (
+  from: number,
+  to: number,
+  ref: string,
+  offset: number
+) => {
+  const refData: any[] = initialData.slice(from - 1, to);
+  let [bottom, top] = [refData[0][ref], refData[0][ref]];
+
+  refData.forEach((d) => {
+    if (d[ref] > top) top = d[ref];
+    if (d[ref] < bottom) bottom = d[ref];
+  });
+
+  return [(bottom | 0) - offset, (top | 0) + offset];
+};
+
+const initialState = {
+  data: initialData,
+  left: 'dataMin',
+  right: 'dataMax',
+  refAreaLeft: '',
+  refAreaRight: '',
+  top: 'dataMax+20',
+  bottom: 'dataMin-20',
+  animation: true,
+};
+
+export default class App extends Component<any, any> {
+  constructor(props: any) {
+    super(props);
+    this.state = initialState;
+  }
+  render() {
+    const {
+      data,
+      left,
+      right,
+      refAreaLeft,
+      refAreaRight,
+      top,
+      bottom,
+    } = this.state;
+
+    console.log(this.state);
+
+    return (
+      <div className="highlight-bar-charts" style={{ userSelect: 'none'}}>
+        <LineChart
+          width={800}
+          height={400}
+          data={data}
+          onMouseDown={(e: any) =>
+            this.setState({ refAreaLeft: e.activeLabel })
+          }
+          onMouseMove={(e: any) =>
+            this.state.refAreaLeft &&
+            this.setState({ refAreaRight: e.activeLabel })
+          }
+        >
+          <CartesianGrid vertical={false} opacity="40%" />
+          <XAxis
+            allowDataOverflow
+            dataKey="name"
+            domain={[left, right]}
+            type="number"
+          />
+          <YAxis
+            orientation="left"
+            allowDataOverflow
+            domain={[bottom, top]}
+            type="number"
+            yAxisId="1"
+          />
+          <Tooltip />
+          <Line
+            yAxisId="1"
+            type="natural"
+            dataKey="impression"
+            stroke="#ff0000"
+            animationDuration={300}
+          />
+        </LineChart>
+      </div>
+    );
+  }
 }
-export default Chart
