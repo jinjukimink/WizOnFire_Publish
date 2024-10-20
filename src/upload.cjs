929966d4f605a56d2ffb92503832dae0a133a6f6
jinjukim <kimjinjuind1@hanyang.ac.kr>
2024-10-20 23:10:17 +0900

    feat: 코치디테일 json 추가

--- src/upload.cjs
+++ src/upload.cjs
@@ -19,14 +19,29 @@ const database = getDatabase(firebaseApp);
 
 async function uploadJSONData() {
     const jsonFiles = [
-      'src/json/player/coachlist.json',
-      'src/json/player/catcherlist.json',
-      'src/json/player/infielderlist.json',
-      'src/json/player/outfielderlist.json',
-      'src/json/player/pitcherlist.json',
-      'src/json/player/cheerleader.json',
-      'src/json/landing/recentGame.json',
-      'src/json/landing/teamranking.json'
+      'src/json/player/coachdetail/60100.json',
+      'src/json/player/coachdetail/70408.json',
+      'src/json/player/coachdetail/70553.json',
+      'src/json/player/coachdetail/71835.json',
+      'src/json/player/coachdetail/72801.json',
+
+      'src/json/player/coachdetail/73136.json',
+      'src/json/player/coachdetail/73228.json',
+      'src/json/player/coachdetail/74339.json',
+      'src/json/player/coachdetail/75668.json',
+      'src/json/player/coachdetail/77609.json',
+
+      'src/json/player/coachdetail/77654.json',
+      'src/json/player/coachdetail/77733.json',
+      'src/json/player/coachdetail/79191.json',
+      'src/json/player/coachdetail/89620.json',
+      'src/json/player/coachdetail/90025.json',
+
+      'src/json/player/coachdetail/92401.json',
+      'src/json/player/coachdetail/94415.json',
+      'src/json/player/coachdetail/94843.json',
+      'src/json/player/coachdetail/97351.json',
+      'src/json/player/coachdetail/75149.json'
     ]; // 경로 확인
 
     for (const file of jsonFiles) {
@@ -39,7 +54,7 @@ async function uploadJSONData() {
             if (file.includes('recentGame') || file.includes('teamranking')) {
               newRef = ref(database,`game/${fileName}`); 
             } else {
-              newRef = ref(database,`player/${fileName}`); 
+              newRef = ref(database,`player/coachdetail/${fileName}`); 
             }
             await set(newRef, data);
             console.log(`Uploaded ${file} successfully.`);
